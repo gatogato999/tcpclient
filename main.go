3efd12ebe4d62b88cd8239a4691afff5b69551b3
gatogato999 <ggaattoo38@gmail.com>
2026-02-20 08:50:12 +0200

    added : args from user
    ./app <address:port> <method> <route to call>

--- main.go
+++ main.go
@@ -5,15 +5,24 @@ import (
 	"io"
 	"log"
 	"net"
+	"os"
 )
 
 func main() {
-	tcpConnObject, err := net.Dial("tcp", ":10055")
+	if len(os.Args) < 4 {
+		log.Fatal("needs more args : <address:port> <method> <route> ")
+	}
+
+	address := os.Args[1]
+	meth := os.Args[2]
+	route := os.Args[3]
+
+	tcpConnObject, err := net.Dial("tcp", address)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	_, err = tcpConnObject.Write([]byte("GET /marks/login?= HTTP/1.0\r\n\r\n"))
+	_, err = fmt.Fprintf(tcpConnObject, "%s %s HTTP/1.0\r\n\r\n", meth, route)
 	if err != nil {
 		log.Fatal("Error writing to server:", err)
 	}
